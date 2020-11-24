f8eeebe29f53d1c156f9e1af690d9dd5a649b489
FoxxMD <FoxxMD@users.noreply.github.com>
2020-11-24 15:06:10 -0500

    Fix splat arg usage for now
    Should be able to just throw arguments at it but for some reason it needs to be in an object for now?

--- sources/PlexSource.js
+++ sources/PlexSource.js
@@ -70,17 +70,17 @@ export default class PlexSource {
         } = playObj;
 
         if (this.users !== undefined && user !== undefined && !this.users.includes(user)) {
-            this.logger.debug(`Will not scrobble webhook event because author was not an allowed user: ${user}`, artist, track)
+            this.logger.debug(`Will not scrobble webhook event because author was not an allowed user: ${user}`, { artist, track })
             return false;
         }
 
         if (event !== 'media.scrobble') {
-            this.logger.debug(`Will not scrobble webhook event because it is not media.scrobble (${event})`, artist, track)
+            this.logger.debug(`Will not scrobble webhook event because it is not media.scrobble (${event})`, { artist, track })
             return false;
         }
 
         if (mediaType !== 'track') {
-            this.logger.debug(`Will not scrobble webhook event because media type was not a track (${mediaType})`, artist, track);
+            this.logger.debug(`Will not scrobble webhook event because media type was not a track (${mediaType})`, { artist, track });
             return false;
         }
 
