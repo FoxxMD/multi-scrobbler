5dc54849cc8ba064c1b5c18c90be2e0eb294b212
FoxxMD <FoxxMD@users.noreply.github.com>
2023-07-13 15:51:20 -0400

    chore(github): Add action for build/deploy docusaurus docsite

--- docsite/docusaurus.config.js
+++ docsite/docusaurus.config.js
@@ -11,15 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://foxxmd.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/multi-scrobbler',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'foxxmd', // Usually your GitHub org/user name.
+  projectName: 'multi-scrobbler', // Usually your repo name.
+
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
