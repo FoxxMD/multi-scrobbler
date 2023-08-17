7dffc3def706d5b20777fcf8db1c1637a07fc2dc
FoxxMD <FoxxMD@users.noreply.github.com>
2023-08-17 13:17:38 -0400

    Don't recompile tsx (front end only)

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -23,7 +23,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
+                test: /\.ts$/,
                 use: [
                     {
                         loader: "ts-loader",
