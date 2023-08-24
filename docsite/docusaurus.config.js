4cccdeeff069741f4b67ab5c916787a26fb5605a
FoxxMD <FoxxMD@users.noreply.github.com>
2023-08-24 10:26:45 -0400

    chore: Clean up site metadata and update logo
    * Changed logo color to be easy to view in both dark/light modes (better color in general, imo)
    * Changed and cleaned up head/meta/manifest info and logos for docs and frontend

--- docsite/docusaurus.config.js
+++ docsite/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Multi-Scrobbler',
+  tagline: 'Scrobble all the things',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -106,6 +106,10 @@ const config = {
           {
             title: 'Docs',
             items: [
+              {
+                label: 'Overview',
+                to: '/',
+              },
               {
                 label: 'Installation',
                 to: '/docs/installation',
--- start-client.js
+++
@@ -1,3 +0,0 @@
-const args = [ 'start' ];
-const opts = { stdio: 'inherit', cwd: 'web', shell: true };
-require('child_process').spawn('yarn', args, opts);
