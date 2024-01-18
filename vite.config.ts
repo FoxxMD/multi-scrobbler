86611d764d9f39f2fd89675d8352e6637bdd9b75
FoxxMD <FoxxMD@users.noreply.github.com>
2024-01-18 14:36:28 -0500

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-// @ts-ignore
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
         plugins: [react()],
+        build: {
+            sourcemap: true
+        }
     };
 });
