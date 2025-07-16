ce39065c807c19cc2e6f4dcf87be3e97f593413f
FoxxMD <FoxxMD@users.noreply.github.com>
2025-07-16 13:33:10 +0000

    docs: Fix social card image

--- docsite/docusaurus.config.ts
+++ docsite/docusaurus.config.ts
@@ -92,7 +92,7 @@ const config: Config = {
   themeConfig:
     {
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/icon.png',
       navbar: {
         title: 'Multi-Scrobbler',
         hideOnScroll: true,
