a0bdd15d152c6fca5911046903b6ce2094a831af
FoxxMD <FoxxMD@users.noreply.github.com>
2025-10-30 15:19:01 +0000

    docs: Move landing page to docs to simply url structure
    Set docs as base for router so there isn't an additional docs in url

--- docsite/docs/index.mdx
+++ docsite/docs/index.mdx
@@ -1,5 +1,6 @@
 ---
 title: Overview
+slug: /
 ---
 
 # Overview
--- docsite/docusaurus.config.ts
+++ docsite/docusaurus.config.ts
@@ -49,6 +49,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
