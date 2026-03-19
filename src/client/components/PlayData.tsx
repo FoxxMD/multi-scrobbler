943bdc3e88ef525c16aff0afff13faf684396991
FoxxMD <FoxxMD@users.noreply.github.com>
2026-03-19 20:23:15 +0000

    feat(ui): Improve play data

--- src/client/components/PlayData.tsx
+++ src/client/components/PlayData.tsx
@@ -1,11 +1,10 @@
 import React, { Fragment, useMemo, useState } from 'react';
-import { EmptyState, DataList, HStack, Tag, Wrap, Box, Flex, SegmentGroup, Stack, Text, Separator, IconButton, Container, SimpleGrid } from "@chakra-ui/react"
+import { EmptyState, DataList, HStack, Tag, Tabs, Wrap, Box, Flex, SegmentGroup, Stack, Text, Separator, IconButton, Container, SimpleGrid, Float } from "@chakra-ui/react"
 import { LuCode, LuText } from "react-icons/lu"
 import { JsonPlayObject } from '../../core/Atomic.js';
 import { shortTodayAwareFormat } from '../../core/TimeUtils.js';
 import dayjs from 'dayjs';
 import { ChakraCodeBlock } from './CodeBlock.js';
-import { safeStringify } from '../../core/StringUtils.js';
 import { TextMuted } from './TextMuted.js';
 
 const EmptyPlayData = () => {
@@ -21,13 +20,14 @@ const EmptyPlayData = () => {
     );
 }
 
+export type DisplayDates = false | 'all' | 'played' | 'seen';
+
 export interface PlayInfoProps {
     play?: JsonPlayObject
     final?: JsonPlayObject
     showCodeToggle?: boolean
     showCompare?: boolean
     compareDefault?: 'Initial' | 'Final'
-    datesFooter?: false | 'all' | 'played' | 'seen'
     dates?: false | 'all' | 'played' | 'seen'
 }
 
@@ -38,7 +38,6 @@ export const PlayData = (props?: PlayInfoProps) => {
         showCodeToggle = true,
         showCompare = true,
         compareDefault = 'Initial',
-        datesFooter = false,
         dates = 'all'
     } = props ?? {};
 
@@ -46,61 +45,62 @@ export const PlayData = (props?: PlayInfoProps) => {
         return <EmptyPlayData />
     }
 
-    const [compareVal, setCompareVal] = useState(compareDefault)
     const [codeMode, setCodeMode] = useState(false);
 
-    const displayedPlay = compareVal === 'Initial' ? play : final;
+    let code: JSX.Element | null = null;
 
-    let comparer: JSX.Element | null = null,
-        copy: JSX.Element | null = null;
+    const comparable = showCompare && final !== undefined;
 
-    if (showCompare && final !== undefined) {
-        comparer = (
-            <SegmentGroup.Root size="xs" value={compareVal} onValueChange={(e) => setCompareVal(e.value as 'Initial' | 'Final')}>
-                <SegmentGroup.Indicator />
-                <SegmentGroup.Items items={["Initial", "Final"]} />
-            </SegmentGroup.Root>
-        );
-    }
     if (showCodeToggle) {
-        copy = (
+        code = (
             <IconButton variant="outline" size="xs" onClick={() => setCodeMode(!codeMode)}>
                 {codeMode ? <LuText /> : <LuCode />}
             </IconButton>
         );
     }
 
-    const datesFooterContent = useMemo(() => {
-        let dateElm: JSX.Element;
-        if (datesFooter !== false) {
-            let playDate: JSX.Element,
-                seenDate: JSX.Element;
-            if (play.data.playDate !== undefined && ['all', 'played'].includes(datesFooter)) {
-                playDate = <Text textStyle="xs" color="fg.muted">{`Played ${shortTodayAwareFormat(dayjs(play.data.playDate))}`}</Text>
-            }
-            // TODO implement seenAt for play data
-            if (play.data.playDateCompleted !== undefined && ['all', 'seen'].includes(datesFooter)) {
-                seenDate = <Text textStyle="xs" color="fg.muted">{`Seen ${shortTodayAwareFormat(dayjs(play.data.playDateCompleted))}`}</Text>
-            }
-            if (playDate !== undefined && seenDate !== undefined) {
-                dateElm = <HStack gap="1">{playDate}<Separator orientation="vertical" height="4" />{seenDate}</HStack>
-            } else if (playDate !== undefined) {
-                dateElm = playDate;
-            } else if (seenDate !== undefined) {
-                dateElm = seenDate;
-            }
-        }
-        return dateElm;
-    }, [play, datesFooter]);
+    if (!comparable) {
+        return (<Box position="relative">
+            <Float placement="top-end" offset="3" hideBelow="sm" zIndex={100}>{code}</Float>
+            {codeMode ? <ChakraCodeBlock code={play} /> : <PlayDataDataList play={play} dates={dates} />}
+        </Box>);
+    }
+
+    return (
+        <Box position="relative">
+            <Float placement="top-end" offset="3" hideBelow="sm" zIndex={100}>{code}</Float>
+            <Tabs.Root  defaultValue={compareDefault}>
+                <Tabs.List>
+                    <Tabs.Trigger value="Initial">Initial</Tabs.Trigger>
+                    <Tabs.Trigger value="Final">Final</Tabs.Trigger>
+                </Tabs.List>
+                <Tabs.Content value="Initial">
+                    {codeMode ? <ChakraCodeBlock code={play} /> : <PlayDataDataList play={play} dates={dates} />}
+                </Tabs.Content>
+                <Tabs.Content value="Final">
+                    {codeMode ? <ChakraCodeBlock code={final} /> : <PlayDataDataList play={final} dates={dates} />}
+                </Tabs.Content>
+            </Tabs.Root>
+        </Box>
+    );
+}
+
+export const PlayDataDataList = (props: { play: JsonPlayObject, dates: DisplayDates }) => {
+
+    const {
+        play,
+        dates
+    } = props;
+
 
     let albumArtistElm: JSX.Element;
 
-    if (displayedPlay.data.albumArtists !== undefined && displayedPlay.data.albumArtists.length > 0) {
+    if (play.data.albumArtists !== undefined && play.data.albumArtists.length > 0) {
         albumArtistElm = (
             <DataList.Item>
                 <DataList.ItemLabel>Album Artists</DataList.ItemLabel>
                 <DataList.ItemValue>
-                    <HStack>{displayedPlay.data.albumArtists.map((x, index) => {
+                    <HStack>{play.data.albumArtists.map((x, index) => {
                         return (
                             <Tag.Root key={index}>
                                 <Tag.Label>{x}</Tag.Label>
@@ -116,20 +116,67 @@ export const PlayData = (props?: PlayInfoProps) => {
         data: {
             artists = []
         } = {}
-    } = displayedPlay;
+    } = play;
+
+    return (
+        <Flex gap="2" wrap="wrap">
+            <Box marginEnd="auto">
+                <DataList.Root>
+                    <DataList.Item>
+                        <DataList.ItemLabel flexShrink="1">Title</DataList.ItemLabel>
+                        <DataList.ItemValue>{play.data.track}</DataList.ItemValue>
+                    </DataList.Item>
+                    <DataList.Item>
+                        <DataList.ItemLabel>Artists</DataList.ItemLabel>
+                        <DataList.ItemValue>
+                            {artists.length === 0 ? <Text color="fg.muted">(No Artists)</Text> :
+                                <HStack>{play.data.artists.map((x, index) => {
+                                    return (
+                                        <Tag.Root key={index}>
+                                            <Tag.Label>{x}</Tag.Label>
+                                        </Tag.Root>
+                                    );
+                                })}</HStack>}
+                        </DataList.ItemValue>
+                    </DataList.Item>
+                    {albumArtistElm}
+                    <DataList.Item>
+                        <DataList.ItemLabel>Album</DataList.ItemLabel>
+                        <DataList.ItemValue>{play.data.album}</DataList.ItemValue>
+                    </DataList.Item>
+                </DataList.Root>
+            </Box>
+            <Box>
+                <DataList.Root>
+                    <PlayDatesStack play={play} dates={dates} />
+                </DataList.Root>
+            </Box>
+        </Flex>
+    )
+}
+
+export const PlayInfoContainer = (props?: PlayInfoProps) => {
+    return <Container maxWidth="lg"><PlayData {...props} /></Container>
+}
+
+export const PlayDatesStack = (props: { play: JsonPlayObject, dates: DisplayDates }) => {
+    const {
+        play,
+        dates
+    } = props;
 
     let datesItem: JSX.Element | null;
-    if(dates === false) {
+    if (dates === false) {
         datesItem = null;
     } else {
         const dateElements = [];
-        if(dates.includes('played') || dates.includes('all')) {
+        if (dates.includes('played') || dates.includes('all')) {
             dateElements.push((<TextMuted>{`Played ${shortTodayAwareFormat(dayjs(play.data.playDate))}`}</TextMuted>));
-            if(play.data.playDateCompleted !== undefined) {
+            if (play.data.playDateCompleted !== undefined) {
                 dateElements.push((<TextMuted>{`Played Until ${shortTodayAwareFormat(dayjs(play.data.playDateCompleted))}`}</TextMuted>));
             }
         }
-        if(dates.includes('seen') || dates.includes('all')) {
+        if (dates.includes('seen') || dates.includes('all')) {
             dateElements.push((<TextMuted>{`Seen ${shortTodayAwareFormat(dayjs(play.data.playDate))}`}</TextMuted>));
         }
         datesItem = (
@@ -144,55 +191,35 @@ export const PlayData = (props?: PlayInfoProps) => {
         )
     }
 
-    return (
-        <Fragment>
-            <Stack gap="3">
-                <Flex justify="space-between">
-                   <Box>{comparer}</Box> <Box>{copy}</Box>
-                </Flex>
-                {
-                    codeMode ? <ChakraCodeBlock code={safeStringify(displayedPlay)} /> : (
-                        <Flex gap="2" wrap="wrap">
-                            <Box marginEnd="auto">
-                                <DataList.Root>
-                                    <DataList.Item>
-                                        <DataList.ItemLabel flexShrink="1">Title</DataList.ItemLabel>
-                                        <DataList.ItemValue>{displayedPlay.data.track}</DataList.ItemValue>
-                                    </DataList.Item>
-                                    <DataList.Item>
-                                        <DataList.ItemLabel>Artists</DataList.ItemLabel>
-                                        <DataList.ItemValue>
-                                            {artists.length === 0 ? <Text color="fg.muted">(No Artists)</Text> :
-                                                <HStack>{displayedPlay.data.artists.map((x, index) => {
-                                                    return (
-                                                        <Tag.Root key={index}>
-                                                            <Tag.Label>{x}</Tag.Label>
-                                                        </Tag.Root>
-                                                    );
-                                                })}</HStack>}
-                                        </DataList.ItemValue>
-                                    </DataList.Item>
-                                    {albumArtistElm}
-                                    <DataList.Item>
-                                        <DataList.ItemLabel>Album</DataList.ItemLabel>
-                                        <DataList.ItemValue>{displayedPlay.data.album}</DataList.ItemValue>
-                                    </DataList.Item>
-                                </DataList.Root>
-                            </Box>
-                            <Box>
-                                <DataList.Root>
-                                    {datesItem}
-                                </DataList.Root>
-                            </Box>
-                            {datesFooterContent}
-                        </Flex>
-                    )
-                }
-            </Stack>
-        </Fragment>
-    )
+    return datesItem;
 }
 
-export const PlayInfoContainer = (props?: PlayInfoProps) => {
-    return <Container maxWidth="lg"><PlayData {...props} /></Container>
+export const PlayDatesFooter = (props: { play: JsonPlayObject, dates: DisplayDates }) => {
+
+    const {
+        play,
+        dates
+    } = props;
+
+    let dateElm: JSX.Element;
+
+    if (dates !== false) {
+        let playDate: JSX.Element,
+            seenDate: JSX.Element;
+        if (play.data.playDate !== undefined && ['all', 'played'].includes(dates)) {
+            playDate = <Text textStyle="xs" color="fg.muted">{`Played ${shortTodayAwareFormat(dayjs(play.data.playDate))}`}</Text>
+        }
+        // TODO implement seenAt for play data
+        if (play.data.playDateCompleted !== undefined && ['all', 'seen'].includes(dates)) {
+            seenDate = <Text textStyle="xs" color="fg.muted">{`Seen ${shortTodayAwareFormat(dayjs(play.data.playDateCompleted))}`}</Text>
+        }
+        if (playDate !== undefined && seenDate !== undefined) {
+            dateElm = <HStack gap="1">{playDate}<Separator orientation="vertical" height="4" />{seenDate}</HStack>
+        } else if (playDate !== undefined) {
+            dateElm = playDate;
+        } else if (seenDate !== undefined) {
+            dateElm = seenDate;
+        }
+    }
+    return dateElm;
 }
