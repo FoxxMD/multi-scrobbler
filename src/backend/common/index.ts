8cffd0d06808477e0bb153d50c121a23f11883fa
FoxxMD <FoxxMD@users.noreply.github.com>
2026-05-08 02:57:22 +0000

    fix: Fix resolving config globally
    #588

--- src/backend/common/index.ts
+++ src/backend/common/index.ts
@@ -5,4 +5,4 @@ import * as path from 'path';
 //const __dirname = path.dirname(__filename);
 
 export const projectDir = process.cwd(); //path.resolve(__dirname, '../../../');
-export const configDir: string = path.resolve(projectDir, './config');
+export const configDir: string = process.env.CONFIG_DIR || path.resolve(projectDir, `./config`);
