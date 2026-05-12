1f199e28ef213a505358716b579a609b1cc33d6c
FoxxMD <FoxxMD@users.noreply.github.com>
2026-05-12 14:44:11 +0000

    fix(ui): Correct for bad ansi export

--- src/client/logs/LogLine.tsx
+++ src/client/logs/LogLine.tsx
@@ -1,5 +1,8 @@
 import React, {PropsWithChildren} from 'react';
-import Ansi from "ansi-to-react";
+import * as AnsiImport from "ansi-to-react";
+
+// @ts-expect-error Ansi export is built incorrectly
+const Ansi = AnsiImport.default.default as typeof AnsiImport.default;
 
 const LogLine = (props: PropsWithChildren<{level: number, levelLabel: string, message: string}>) => {
     return (
